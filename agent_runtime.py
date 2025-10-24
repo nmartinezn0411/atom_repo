81c0c1fea2d5d8997de4536ba5bf86efb5549fbc
nmartinezn0411 <nmartinezn0411@gmail.com>
2025-10-23 20:20:26 -0400

    some changes for railway

--- agent_runtime.py
+++ agent_runtime.py
@@ -60,6 +60,7 @@ TOOLS = [query_tool]
 # Modelo
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
+    google_api_key = os.getenv("GOOGLE_API_KEY"),
     temperature=0,
     verbose=True,
 ).bind_tools(TOOLS)
