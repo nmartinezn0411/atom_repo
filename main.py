aad98ea5f66e5da2676190da74603827c9544203
nmartinezn0411 <nmartinezn0411@gmail.com>
2025-10-23 20:08:16 -0400

    some changes for railway

--- main.py
+++ main.py
@@ -77,3 +77,7 @@ def ask(req: AskRequest, api_key: str = Depends(validate_api_key)):
     }
 
     return payload
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
